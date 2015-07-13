f3766c2fb43347b017003a8a51251b30b7fa3f99
Enrique Pineda <enrique.pineda@gmail.com>
2015-07-12 20:39:11 -0400

    css. test data. does not render. (it sucks.)

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,10 @@
-angular.module('tic-tac-toe', ['ngGrid'])
+var app = angular.module('tic-tac-toe', ['ngGrid'])
+
+app.controller('GameCtrl', ['$scope', function($scope) {
+    $scope.gameData = [
+        { col1:'X', col2:'O', col3:'X' }
+    ]
+    $scope.gridOptions = {
+        data: 'gameData'
+    }
+}])
