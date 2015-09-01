c8327a78f14b61567d48e2193c734732f8d0ce33
Enrique Pineda <enrique.pineda@gmail.com>
2015-09-01 18:41:59 -0400

    beginning of player management

--- controllers.js
+++ controllers.js
@@ -1,5 +1,6 @@
 angular.module('tic-tac-toe.controllers', ['firebase', 'vesparny.fancyModal'])
-.controller('GameCtrl', ['$fancyModal', function($fancyModal) {
+.controller('GameCtrl', ['$fancyModal', '$scope', function($fancyModal, $scope) {
+    $scope.player = {}
     $fancyModal.open({ templateUrl: 'modal.html' })
 /*
     if (opponent.found) this is a firebase object e.g. { location:'us', marker:'O', found:true }
