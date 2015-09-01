4571d6398ee2b89ccc662c119c408319e94aa7bd
Enrique Pineda <enrique.pineda@gmail.com>
2015-09-01 18:19:57 -0400

    Game service

--- services.js
+++ services.js
@@ -1,5 +1,17 @@
 angular.module('tic-tac-toe.services', [])
 .constant('URL', 'https://tictactoe-eppineda.firebaseio.com')
+.constant('X', 'X')
+.constant('O', 'O')
+.factory('Game', [function() {
+    return {
+        join:function(playerName) {
+            // assigned 'O' -- second player
+        },
+        create:function(playerName) {
+            // assigned 'X' -- first player
+        }
+    }
+}])
 .factory('FirebaseAccess', ['$firebaseObject', function($firebaseObject) {
     var refGames = new Firebase(URL + '/games')
     var refWaiting = new Firebase(URL + '/waiting')
