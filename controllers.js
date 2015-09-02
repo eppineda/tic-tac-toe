c7d7c4ac492d90f5c835d1e2f03fb725b418451a
Enrique Pineda <enrique.pineda@gmail.com>
2015-09-02 05:08:53 -0400

    retrieve all players waiting

--- controllers.js
+++ controllers.js
@@ -1,7 +1,25 @@
 angular.module('tic-tac-toe.controllers', ['firebase', 'vesparny.fancyModal'])
-.controller('GameCtrl', ['$fancyModal', '$scope', function($fancyModal, $scope) {
+.controller('GameCtrl', [
+'$fancyModal',
+'$scope',
+'Game',
+function($fancyModal, $scope, Game) {
     $scope.player = {}
     $fancyModal.open({ templateUrl: 'modal.html' })
+
+    Game.join($scope.player.name).then(
+        function(success) {
+            var waiting = success
+            // todo: get next person waiting and create game with that person
+            console.log(waiting)
+        },
+        function(failure) {
+            console.error(failure)
+        },
+        function(update){
+            console.log(update)
+        }
+    )
 /*
     if (opponent.found) this is a firebase object e.g. { location:'us', marker:'O', found:true }
         $fancyModal.close()
--- services.js
+++ services.js
@@ -1,5 +1,5 @@
 angular.module('tic-tac-toe.services', [])
-.constant('URL', 'https://tictactoe-eppineda.firebaseio.com')
+.constant('FIREBASE_URL', 'https://tictactoe-eppineda.firebaseio.com')
 .constant('X', 'X')
 .constant('O', 'O')
 .constant('TIMEOUT', 60)
@@ -18,6 +18,18 @@ function(
     return {
         join:function(playerName) {
             // assigned 'O' -- second player
+            var deferred = $q.defer()
+            var waiting = $firebaseArray(FirebaseAccess.waiting())
+
+            $timeout(function() {
+                deferred.notify('retrieving names of players already waiting')
+                if (1 > waiting.length)
+                    deferred.reject(waiting.length)
+                else {
+                    deferred.resolve(waiting)
+                }
+            }, 1500)
+            return deferred.promise
         },
         create:function(playerName) {
             // assigned 'X' -- first player
@@ -25,9 +37,11 @@ function(
         playAgain:function() {}
     }
 }])
-.factory('FirebaseAccess', ['$firebaseObject', function($firebaseObject) {
-    var refGames = new Firebase(URL + '/games')
-    var refWaiting = new Firebase(URL + '/waiting')
+.factory('FirebaseAccess', [
+'FIREBASE_URL',
+function(FIREBASE_URL) {
+    var refGames = new Firebase(FIREBASE_URL + '/games')
+    var refWaiting = new Firebase(FIREBASE_URL + '/waiting')
     return {
         games:function() { return refGames},
         waiting:function() { return refWaiting }
