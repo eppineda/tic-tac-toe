de49ad1e8c5b7fcef5ccca8875c9c43f256041fe
Enrique Pineda <enrique.pineda@gmail.com>
2015-09-02 08:07:05 -0400

    no other players. create game when someone shows up.

--- controllers.js
+++ controllers.js
@@ -15,6 +15,27 @@ function($fancyModal, $scope, Game) {
         },
         function(failure) {
             console.error(failure)
+
+            var waitingList = [] // todo: firebase
+            var continueWaiting = function() {
+                // todo: ask to continue waiting for another player
+                return false
+            }
+            var playerJoined = function(waitingList) {
+                if ([] === waitingList) return false
+            }
+
+            while (continueWaiting()) {
+                $timeout(function() {
+                    if (playerJoined(waitingList)) {
+                        Game.create($scope.player.name).then(
+                            function(success) {},
+                            function(failure) {},
+                            function(update) {}
+                        )
+                    }
+                }, 60000)
+            } // while
         },
         function(update){
             console.log(update)
